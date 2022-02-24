87082b6c7b173eb28a9c4f916799572cbc7d6e29
Michael Marlowe <marlowemich@gmail.com>
2022-02-24 11:24:22 -0500

    back to top button added

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,16 @@
+const button = document.getElementById("topnav");
+const wrapper = document.querySelector(".wrapper");
 
+// button.attr('style', 'display: none')
+button.addEventListener('click', function(){
+    wrapper.scrollTo(0, 0);
+});
 
+wrapper.addEventListener('scroll', function (){
+    if (wrapper.scrollY > 1000){
+    button.setAttribute('visibility', 'visible')
+}
+    else {
+        button.setAttribute('visibility', 'hidden')
+    }
+});
